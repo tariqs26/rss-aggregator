97b4806c0bb24dcba1c60b46d54f1c924076f321
Saad Tariq <saadtariq.dev@outlook.com>
2024-08-05 17:46:18 -0400

    feat: add api util for parsing parameter id's

---
+++ internal/util/api.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func GetIntId(idParam string) (int32, error) {
+
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		return 0, fmt.Errorf("error parsing ID: %v", err)
+	}
+
+	return int32(id), nil
+}
