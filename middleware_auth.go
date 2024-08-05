6fa8e5309a9e14089de5b8761b6be569f4c9d9bf
Saad Tariq <saadtariq.dev@outlook.com>
2024-08-05 17:49:09 -0400

    feat: add user middleware

---
+++ middleware_auth.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/tariqs26/rss-aggregator/internal/auth"
+	"github.com/tariqs26/rss-aggregator/internal/database"
+	"github.com/tariqs26/rss-aggregator/internal/util"
+)
+
+type AuthHandler func(http.ResponseWriter, *http.Request, database.User)
+
+func middlewareAuth(handler AuthHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKey, err := auth.GetApiKey(r.Header)
+
+		if err != nil {
+			util.RespondWithError(w, http.StatusBadRequest,
+				fmt.Sprintf("auth error: %v", err),
+			)
+			return
+		}
+
+		user, err := apiConfig.DB.GetUserByApiKey(r.Context(), apiKey)
+
+		if err != nil {
+			util.RespondWithError(w, http.StatusInternalServerError,
+				"Error getting user",
+			)
+			return
+		}
+
+		handler(w, r, user)
+	}
+}
